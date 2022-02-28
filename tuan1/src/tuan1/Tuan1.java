fd1c20a97ad7276f64c28967793384537f75da4e
Lê Na <lena09082016@gmail.com>
2022-02-28 18:07:39 +0700

    bvc

---
+++ tuan1/src/tuan1/Tuan1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tuan1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
